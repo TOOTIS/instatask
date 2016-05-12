3c6918bc886d295805bb562e088f0adbabf68fac
Mohamed Anwer <mohammed.ahmed.anwer@gmail.com>
2016-05-12 02:25:56 +0200

    Hotfix: handline BadRequest

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,10 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-  rescue_from ActionController::ParameterMissing, with: -> { render_400 }
+
   rescue_from ArgumentError,                      with: -> { render_400 }
+  rescue_from ActionController::ParameterMissing, with: -> { render_400 }
+  rescue_from ActionController::BadRequest,       with: -> { render_400 }
   rescue_from ActiveRecord::RecordNotFound,       with: -> { render_404 }
   rescue_from ActionController::RoutingError,     with: -> { render_404 }
 
